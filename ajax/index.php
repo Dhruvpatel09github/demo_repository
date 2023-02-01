6e6e5834556eaccbd567445e6aa03855b8e305d7
dhruv <movieking162000@gmail.com>
2023-02-01 12:09:54 +0530

    tital change

--- ajax/index.php
+++ ajax/index.php
@@ -11,7 +11,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>PHP & Ajax CRUD opration</title>
+  <title>PHP & Ajax CRUD Opration</title>
   <link rel="stylesheet" href="css/style.css">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
